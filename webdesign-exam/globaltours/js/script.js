6767c6f84f50365e95ce6723a8ac78bc975ff31f
JaenyungJeong <rantelet@gmail.com>
2026-06-29 22:43:19 +0900

    1043

--- webdesign-exam/globaltours/js/script.js
+++ webdesign-exam/globaltours/js/script.js
@@ -8,4 +8,21 @@ $(function(){
             $(".sub").stop().slideUp();
         })
         // stop 기능 여러번 마우스엔터 했을 때 컴퓨터가 기억해서 계속 클릭하는 수행 정지
+
+
+        //슬라이드
+        let i=0;
+
+        function slide(){
+            if(i<2){
+                i++;
+            }else{
+                i=0
+            }
+            $(".slide ul").animate({marginLeft:-1200*i}, 1000)
+
+        }
+
+        setInterval(slide, 3000);
+        
 })
